2a6649404fe10f9df31f867daf3c36e97722a68b
krishk6 <Kumar.Krishnamurthy@emc.com>
2017-05-30 12:58:01 +0530

    3rd module complete

--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -27,6 +27,7 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddSingleton(Configuration);
             services.AddSingleton<IGreeter, Greeter>();
         }
@@ -41,12 +42,24 @@ namespace OdeToFood
                 app.UseDeveloperExceptionPage();
             }
 
+
+            //app.UseStaticFiles();
+            //app.UseFileServer();
+
+            app.UseMvcWithDefaultRoute();
+            
+            /**
+            app.UseWelcomePage("/welcome");
+
             app.Run(async (context) =>
             {
+                //throw new Exception("something went wrong");
                 //var message = Configuration["Greeting"];
                 var message = greeter.GetGreeting();
                 await context.Response.WriteAsync(message);
             });
+
+    **/
         }
     }
 }
