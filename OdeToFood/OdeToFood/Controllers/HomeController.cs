7b932c11d699e2b02ba7c000398a23f0411efeff
krishk6 <Kumar.Krishnamurthy@emc.com>
2017-05-31 14:37:39 +0530

    module 5 models and view models

--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +12,22 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
             //var model = new Restaurants() { Id = 1, Name = "The House of Kobe" };
-            var model = _restaurantData.GetAll();
+            //var model = _restaurantData.GetAll();
+            var model = new HomePageViewModel();
 
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetGreeting();
 
             return View(model);
            
---
+++ OdeToFood/OdeToFood/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public string CurrentMessage { get; set; }
+        public IEnumerable<Restaurants> Restaurants { get; set; }
+    }
+}
