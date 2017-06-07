1409efd109229f244ad6f33942a747c13732b5ee
kumarsendhi <kumar.sendhi@gmail.com>
2017-06-06 22:49:04 -0700

    ubuntu identity setup

---
+++ OdeToFood/OdeToFood/Migrations/20170606183851_initialubuntu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OdeToFood.Migrations
+{
+    public partial class initialubuntu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
